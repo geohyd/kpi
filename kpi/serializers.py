geohyd/kpi

commit 1e48704bbbb505a14316a8073cf9fd993f152032

Optimize performance of API asset lists
improving endpoint response time by roughly 10x. Fixes #1422

kpi/serializers.py
@@ -270,6 +270,24 @@ class ObjectPermissionSerializer(serializers.ModelSerializer):
             return content_object.assign_perm(user, perm)
 
 
+class ObjectPermissionNestedSerializer(ObjectPermissionSerializer):
+    '''
+    When serializing a list of permissions inside the object to which they are
+    assigned, omit `content_object` to improve performance significantly
+    '''
+    class Meta(ObjectPermissionSerializer.Meta):
+        fields = (
+            'uid',
+            'kind',
+            'url',
+            'user',
+            'permission',
+            #'content_object',
+            'deny',
+            'inherited',
+        )
+
+
 class AncestorCollectionsSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(
         lookup_field='uid', view_name='collection-detail')
@@ -442,7 +460,7 @@ class AssetSerializer(serializers.HyperlinkedModelSerializer):
     )
     ancestors = AncestorCollectionsSerializer(
         many=True, read_only=True, source='get_ancestors_or_none')
-    permissions = ObjectPermissionSerializer(many=True, read_only=True)
+    permissions = ObjectPermissionNestedSerializer(many=True, read_only=True)
     tag_string = serializers.CharField(required=False, allow_blank=True)
     version_id = serializers.CharField(read_only=True)
     has_deployment = serializers.ReadOnlyField()
@@ -591,17 +609,17 @@ class AssetSerializer(serializers.HyperlinkedModelSerializer):
     def get_deployed_version_id(self, obj):
         if not obj.has_deployment:
             return
-        if obj.asset_versions.filter(deployed=True).exists():
-            if isinstance(obj.deployment.version_id, int):
-                # this can be removed once the 'replace_deployment_ids'
-                # migration has been run
-                v_id = obj.deployment.version_id
-                try:
-                    return obj.asset_versions.get(_reversion_version_id=v_id).uid
-                except ObjectDoesNotExist, e:
-                    return obj.asset_versions.filter(deployed=True).first().uid
-            else:
-                return obj.deployment.version_id
+        if isinstance(obj.deployment.version_id, int):
+            asset_versions_uids_only = obj.asset_versions.only('uid')
+            # this can be removed once the 'replace_deployment_ids'
+            # migration has been run
+            v_id = obj.deployment.version_id
+            try:
+                return asset_versions_uids_only.get(_reversion_version_id=v_id).uid
+            except ObjectDoesNotExist, e:
+                return asset_versions_uids_only.filter(deployed=True).first().uid
+        else:
+            return obj.deployment.version_id
 
     def get_deployment__identifier(self, obj):
         if obj.has_deployment:
@@ -767,6 +785,9 @@ class ExportTaskSerializer(serializers.HyperlinkedModelSerializer):
 
 class AssetListSerializer(AssetSerializer):
     class Meta(AssetSerializer.Meta):
+        # WARNING! If you're changing something here, please update
+        # `Asset.optimize_queryset_for_list()`; otherwise, you'll cause an
+        # additional database query for each asset in the list.
         fields = ('url',
                   'date_modified',
                   'date_created',
@@ -1009,14 +1030,8 @@ class CollectionSerializer(serializers.HyperlinkedModelSerializer):
         # (http://www.django-rest-framework.org/api-guide/fields/#source).
         source='*',
         source_processor=lambda source: CollectionChildrenQuerySet(
-            source).select_related(
-                'owner', 'parent'
-            ).prefetch_related(
-                'permissions',
-                'permissions__permission',
-                'permissions__user',
-                'permissions__content_object',
-            ).all()
+            source
+        ).optimize_for_list()
     )
     permissions = ObjectPermissionSerializer(many=True, read_only=True)
     downloads = serializers.SerializerMethodField()

kpi/deployment_backends/kc_access/shadow_models.py
@@ -77,6 +77,7 @@ class LazyModelGroup:
             date_modified = models.DateTimeField()
             uuid = models.CharField(max_length=32, default=u'')
             last_submission_time = models.DateTimeField(blank=True, null=True)
+            num_of_submissions = models.IntegerField(default=0)
 
             @property
             def hash(self):

kpi/deployment_backends/kc_access/utils.py
@@ -35,10 +35,13 @@ def _trigger_kc_profile_creation(user):
 
 @safe_kc_read
 def instance_count(xform_id_string, user_id):
-    return _models.Instance.objects.filter(deleted_at__isnull=True,
-                                           xform__user_id=user_id,
-                                           xform__id_string=xform_id_string,
-                                           ).count()
+    try:
+        return _models.XForm.objects.only('num_of_submissions').get(
+            id_string=xform_id_string,
+            user_id=user_id
+        ).num_of_submissions
+    except _models.XForm.DoesNotExist:
+        return 0
 
 @safe_kc_read
 def last_submission_time(xform_id_string, user_id):

kpi/models/asset.py
@@ -16,6 +16,7 @@ from django.contrib.contenttypes.fields import GenericRelation
 from django.core.exceptions import MultipleObjectsReturned
 from django.db import models
 from django.db import transaction
+from django.db.models import Prefetch
 from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
 import jsonbfield.fields
@@ -101,7 +102,13 @@ class TagStringMixin:
 
     @property
     def tag_string(self):
-        return ','.join(self.tags.values_list('name', flat=True))
+        try:
+            tag_list = self.prefetched_tags
+        except AttributeError:
+            tag_names = self.tags.values_list('name', flat=True)
+        else:
+            tag_names = [t.name for t in tag_list]
+        return ','.join(tag_names)
 
     @tag_string.setter
     def tag_string(self, value):
@@ -675,9 +682,17 @@ class Asset(ObjectPermissionMixin,
 
     @property
     def version_id(self):
-        latest_version = self.latest_version
-        if latest_version:
-            return latest_version.uid
+        try:
+            latest_versions = self.prefetched_latest_versions
+        except AttributeError:
+            latest_version = self.latest_version
+            if latest_version:
+                return latest_version.uid
+        else:
+            try:
+                return latest_versions[0]
+            except IndexError:
+                return None
 
     @property
     def snapshot(self):
@@ -719,6 +734,41 @@ class Asset(ObjectPermissionMixin,
 
     def __unicode__(self):
         return u'{} ({})'.format(self.name, self.uid)
+
+    @staticmethod
+    def optimize_queryset_for_list(queryset):
+        ''' Used by serializers to improve performance when listing assets '''
+        queryset = queryset.defer(
+            # Avoid pulling these `JSONField`s from the database because:
+            #   * they are stored as plain text, and just deserializing them
+            #     to Python objects is CPU-intensive;
+            #   * they are often huge;
+            #   * we don't need them for list views.
+            'content', 'report_styles'
+        ).select_related(
+            'owner__username',
+        ).prefetch_related(
+            # We previously prefetched `permissions__content_object`, but that
+            # actually pulled the entirety of each permission's linked asset
+            # from the database! For now, the solution is to remove
+            # `content_object` here *and* from
+            # `ObjectPermissionNestedSerializer`.
+            'permissions__permission',
+            'permissions__user',
+            # `Prefetch(..., to_attr='prefetched_list')` stores the prefetched
+            # related objects in a list (`prefetched_list`) that we can use in
+            # other methods to avoid additional queries; see:
+            # https://docs.djangoproject.com/en/1.8/ref/models/querysets/#prefetch-objects
+            Prefetch('tags', to_attr='prefetched_tags'),
+            Prefetch(
+                'asset_versions',
+                queryset=AssetVersion.objects.order_by(
+                    '-date_modified'
+                ).only('uid', 'asset', 'date_modified', 'deployed'),
+                to_attr='prefetched_latest_versions',
+            ),
+        )
+        return queryset
 
 
 class AssetSnapshot(models.Model, XlsExportable, FormpackXLSFormUtils):

kpi/models/collection.py
@@ -221,6 +221,9 @@ class CollectionChildrenQuerySet(object):
     def only(self, *args, **kwargs):
         return self._apply_to_both('only', *args, **kwargs)
 
+    def defer(self, *args, **kwargs):
+        return self._apply_to_both('defer', *args, **kwargs)
+
     def _apply_to_both(self, method, *args, **kwargs):
         qs = self._clone()
         qs.child_collections = getattr(qs.child_collections, method)(
@@ -233,6 +236,21 @@ class CollectionChildrenQuerySet(object):
         qs = CollectionChildrenQuerySet(self.collection)
         qs.child_collections = self.child_collections._clone()
         qs.child_assets = self.child_assets._clone()
+        return qs
+
+    def optimize_for_list(self):
+        def optimize_subquery_for_list(sub_qs):
+            model = sub_qs.model
+            try:
+                optimize_method = model.optimize_queryset_for_list
+            except AttributeError:
+                return sub_qs
+            else:
+                return optimize_method(sub_qs)
+
+        qs = self._clone()
+        qs.child_collections = optimize_subquery_for_list(qs.child_collections)
+        qs.child_assets = optimize_subquery_for_list(qs.child_assets)
         return qs
 
 

kpi/views.py
@@ -813,17 +813,10 @@ class AssetViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
 
     ### CURRENT ENDPOINT
     """
+
     # Filtering handled by KpiObjectPermissionsFilter.filter_queryset()
-    queryset = Asset.objects.select_related(
-        'owner', 'parent'
-    ).prefetch_related(
-        'permissions',
-        'permissions__permission',
-        'permissions__user',
-        'permissions__content_object',
-        # Getting the tag_string is making one query per object, but
-        # prefetch_related doesn't seem to help
-    ).all()
+    queryset = Asset.objects.all()
+
     serializer_class = AssetSerializer
     lookup_field = 'uid'
     permission_classes = (IsOwnerOrReadOnly,)
@@ -843,15 +836,13 @@ class AssetViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
             return AssetSerializer
 
     def get_queryset(self, *args, **kwargs):
-        ''' Really temporary way to exclude a taxing field from the database
-        query when the request instructs us to do so. '''
         queryset = super(AssetViewSet, self).get_queryset(*args, **kwargs)
-        # See also AssetSerializer.get_fields()
-        excludes = self.request.GET.get('exclude', '')
-        excludes = excludes.split(',')
-        if 'content' in excludes:
-            queryset = queryset.defer('content')
-        return queryset
+        if self.action == 'list':
+            return queryset.model.optimize_queryset_for_list(queryset)
+        else:
+            # This is called to retrieve an individual record. How much do we
+            # have to care about optimizations for that?
+            return queryset
 
     def _get_clone_serializer(self, current_asset=None):
         """

commit 2068193f1059a71fcb7ca32c272be5e6d2536591

Fix legacy asset version logic broken by 1e48704b

kpi/serializers.py
@@ -615,9 +615,17 @@ class AssetSerializer(serializers.HyperlinkedModelSerializer):
             # migration has been run
             v_id = obj.deployment.version_id
             try:
-                return asset_versions_uids_only.get(_reversion_version_id=v_id).uid
-            except ObjectDoesNotExist, e:
-                return asset_versions_uids_only.filter(deployed=True).first().uid
+                return asset_versions_uids_only.get(
+                    _reversion_version_id=v_id
+                ).uid
+            except AssetVersion.DoesNotExist:
+                deployed_version = asset_versions_uids_only.filter(
+                    deployed=True
+                ).first()
+                if deployed_version:
+                    return deployed_version.uid
+                else:
+                    return None
         else:
             return obj.deployment.version_id
 