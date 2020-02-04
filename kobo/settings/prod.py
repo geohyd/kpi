790f919acdd5dd76319033436fa4e856169d8452
jdugh <jonathan.durand@anteagroup.fr>
2020-02-04 16:05:54 +0100

    AUTH_LDAP_3_ALWAYS_UPDATE_USER default False value

--- kobo/settings/prod.py
+++ kobo/settings/prod.py
@@ -16,7 +16,7 @@ AUTH_LDAP_USER_ATTR_MAP = {
 if os.environ.get('AUTH_LDAP_1_ACTIVE') and os.environ.get('AUTH_LDAP_1_ACTIVE').lower() == 'true':
     AUTH_LDAP_1_SERVER_URI = os.environ.get('AUTH_LDAP_1_SERVER_URI', '')
     AUTH_LDAP_1_USER_DN_TEMPLATE = os.environ.get('AUTH_LDAP_1_USER_DN_TEMPLATE', '')
-    AUTH_LDAP_1_ALWAYS_UPDATE_USER = os.environ.get('AUTH_LDAP_1_ALWAYS_UPDATE_USER', '')
+    AUTH_LDAP_1_ALWAYS_UPDATE_USER = os.environ.get('AUTH_LDAP_1_ALWAYS_UPDATE_USER', 'False')
     AUTH_LDAP_1_BIND_AS_AUTHENTICATING_USER = os.environ.get('AUTH_LDAP_1_BIND_AS_AUTHENTICATING_USER', '')
     if(not AUTH_LDAP_1_BIND_AS_AUTHENTICATING_USER):
         AUTH_LDAP_1_BIND_DN = os.environ.get('AUTH_LDAP_1_BIND_DN', '')
@@ -27,7 +27,7 @@ if os.environ.get('AUTH_LDAP_1_ACTIVE') and os.environ.get('AUTH_LDAP_1_ACTIVE')
 if os.environ.get('AUTH_LDAP_2_ACTIVE') and os.environ.get('AUTH_LDAP_2_ACTIVE').lower() == 'true':
     AUTH_LDAP_2_SERVER_URI = os.environ.get('AUTH_LDAP_2_SERVER_URI', '')
     AUTH_LDAP_2_USER_DN_TEMPLATE = os.environ.get('AUTH_LDAP_2_USER_DN_TEMPLATE', '')
-    AUTH_LDAP_2_ALWAYS_UPDATE_USER = os.environ.get('AUTH_LDAP_2_ALWAYS_UPDATE_USER', '')
+    AUTH_LDAP_2_ALWAYS_UPDATE_USER = os.environ.get('AUTH_LDAP_2_ALWAYS_UPDATE_USER', 'False')
     AUTH_LDAP_2_BIND_AS_AUTHENTICATING_USER = os.environ.get('AUTH_LDAP_2_BIND_AS_AUTHENTICATING_USER', '')
     if (not AUTH_LDAP_2_BIND_AS_AUTHENTICATING_USER):
         AUTH_LDAP_2_BIND_DN = os.environ.get('AUTH_LDAP_2_BIND_DN', '')
@@ -38,7 +38,7 @@ if os.environ.get('AUTH_LDAP_2_ACTIVE') and os.environ.get('AUTH_LDAP_2_ACTIVE')
 if os.environ.get('AUTH_LDAP_3_ACTIVE') and os.environ.get('AUTH_LDAP_3_ACTIVE').lower() == 'true':
     AUTH_LDAP_3_SERVER_URI = os.environ.get('AUTH_LDAP_3_SERVER_URI', '')
     AUTH_LDAP_3_USER_DN_TEMPLATE = os.environ.get('AUTH_LDAP_3_USER_DN_TEMPLATE', '')
-    AUTH_LDAP_3_ALWAYS_UPDATE_USER = os.environ.get('AUTH_LDAP_3_ALWAYS_UPDATE_USER', '')
+    AUTH_LDAP_3_ALWAYS_UPDATE_USER = os.environ.get('AUTH_LDAP_3_ALWAYS_UPDATE_USER', 'False')
     AUTH_LDAP_3_BIND_AS_AUTHENTICATING_USER = os.environ.get('AUTH_LDAP_3_BIND_AS_AUTHENTICATING_USER', '')
     if (not AUTH_LDAP_3_BIND_AS_AUTHENTICATING_USER):
         AUTH_LDAP_3_BIND_DN = os.environ.get('AUTH_LDAP_3_BIND_DN', '')
