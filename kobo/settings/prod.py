7e649e2220629112678d0440ff7a592fd2883f31
JD2701 <jonathan.durand@anteagroup.com>
2021-04-08 17:42:24 +0200

    update ldap settings

--- kobo/settings/prod.py
+++ kobo/settings/prod.py
@@ -48,15 +48,15 @@ AUTH_LDAP_USER_ATTR_MAP = {
 
 if os.environ.get('AUTH_LDAP_1_ACTIVE') and os.environ.get('AUTH_LDAP_1_ACTIVE').lower() == 'true':
     AUTH_LDAP_1_SERVER_URI = os.environ.get('AUTH_LDAP_1_SERVER_URI', '')
-    AUTH_LDAP_1_USER_DN_TEMPLATE = os.environ.get('AUTH_LDAP_1_USER_DN_TEMPLATE', '')
-    AUTH_LDAP_1_ALWAYS_UPDATE_USER = os.environ.get('AUTH_LDAP_1_ALWAYS_UPDATE_USER', 'False')
-    AUTH_LDAP_1_BIND_AS_AUTHENTICATING_USER = os.environ.get('AUTH_LDAP_1_BIND_AS_AUTHENTICATING_USER', '')
-    if(not AUTH_LDAP_1_BIND_AS_AUTHENTICATING_USER):
+    AUTH_LDAP_1_BIND_AS_AUTHENTICATING_USER = eval(os.environ.get('AUTH_LDAP_1_BIND_AS_AUTHENTICATING_USER', 'False'))
+    if not AUTH_LDAP_1_BIND_AS_AUTHENTICATING_USER:
         AUTH_LDAP_1_BIND_DN = os.environ.get('AUTH_LDAP_1_BIND_DN', '')
         AUTH_LDAP_1_BIND_PASSWORD = os.environ.get('AUTH_LDAP_1_BIND_PASSWORD', '')
+    AUTH_LDAP_1_USER_SEARCH = LDAPSearch(
+        os.environ.get('AUTH_LDAP_1_BASE_DN', ''), ldap.SCOPE_SUBTREE, os.environ.get('AUTH_LDAP_1_SEARCH_FILTER', '')
+    )
+    AUTH_LDAP_1_ALWAYS_UPDATE_USER = eval(os.environ.get('AUTH_LDAP_1_ALWAYS_UPDATE_USER', 'False'))
     AUTH_LDAP_1_USER_ATTR_MAP = AUTH_LDAP_USER_ATTR_MAP
-    if os.environ.get('AUTH_LDAP_1_OPT_REFERRALS'):
-        AUTH_LDAP_1_CONNECTION_OPTIONS = {ldap.OPT_REFERRALS: os.environ.get('AUTH_LDAP_1_OPT_REFERRALS')}
     AUTHENTICATION_BACKENDS = ("kpi.backends_ldap.LDAPBackend1",) + AUTHENTICATION_BACKENDS
 
 if os.environ.get('AUTH_LDAP_2_ACTIVE') and os.environ.get('AUTH_LDAP_2_ACTIVE').lower() == 'true':
